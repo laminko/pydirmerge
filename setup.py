7aba5f207b57c6f065feb4a9f58895c6d441d724
lmk <lmk@localhost>
2012-05-18 01:34:16 +0630

    modifying folder

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
-from distutils.core import setup
 
-setup(name='pydirmerge',
-      version='1.0.0',
-      description='Python directories merger',
-      author='lmk',
-      author_email='lminko.lmk@gmail.com',
-      url='http://code.google.com/p/pydirmerge',
-      scripts=['pydirmerge.py'],
-     )
+def main():
+    from distutils.core import setup
+    
+    setup(name='pydirmerge',
+          version='1.0.0',
+          description='Python directories merger',
+          author='lmk',
+          author_email='lminko.lmk@gmail.com',
+          url='http://code.google.com/p/pydirmerge',
+          scripts=['pydirmerge.py'],
+         )
+    
+if __name__ == '__main__':
+    main()
