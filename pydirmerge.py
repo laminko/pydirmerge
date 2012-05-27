5b3e668e3af3cbcd624393c55ecc2a2a8a313097
lmk <lmk@localhost>
2012-05-27 21:39:07 +0630

    added similarity.

--- pydirmerge.py
+++ pydirmerge.py
@@ -4,6 +4,9 @@ import sys
 import os
 import shutil
 import traceback
+import difflib
+
+from datetime import datetime
 
 class appy(object):
     
@@ -18,6 +21,8 @@ class dirMerger(object):
         self._input_dirs = []
         self._output_dir = ''
         self._verbose = False
+        self._duplicatefiles = ['Source, Destination, Similarity']
+        self._logfile = 'log_%s.log' % datetime.now().isoformat()
         
     def recursiveFDs(self, path):
         filepaths = []
@@ -36,6 +41,8 @@ class dirMerger(object):
                 os.makedirs(destination)
         
     def moveFiles(self, files, old_path, new_path):
+        #f = each source file
+        sm = difflib.SequenceMatcher()
         for f in files:
             destination = f.replace(old_path, new_path)
             if not os.path.exists(destination):
@@ -43,16 +50,35 @@ class dirMerger(object):
                     print 'Copying: from %s to %s' % (f, destination)
                 shutil.copyfile(f, destination)
             else:
+                sm.set_seqs(open(f).read(), open(destination).read())
+                similarity = round(sm.quick_ratio(), 2) * 100
+                self._duplicatefiles.append('%s, %s, %s' % (f, destination, similarity))
                 if self._verbose:
-                    print 'Skipped: %s' % f
+                    print 'Duplicate file found!'
         
     def mergeDirs(self):
         print 'Started ...'
         for idir in self._input_dirs:
-                fs, ds = self.recursiveFDs(idir) #FILEs, DIRs
-                self.createDirs(ds, idir, self._output_dir)
-                self.moveFiles(fs, idir, self._output_dir)
+            fs, ds = self.recursiveFDs(idir) #FILEs, DIRs
+            self.createDirs(ds, idir, self._output_dir)
+            self.moveFiles(fs, idir, self._output_dir)
         print 'Finished ...'
+        
+    def printProlog(self):
+        print 'Sources: \n%s\n\nDestination: \n%s' \
+        % ('\n'.join(self._input_dirs), self._output_dir)
+        
+    def printEpilog(self):
+        print '''Total duplicate found: %d
+        Log file path: %s
+        \n[See log file to view duplicate files.]''' \
+        % (len(self._duplicatefiles) - 1, os.path.join(self._output_dir, self._logfile))
+        
+    def writeLog(self):
+        print 'Writing log ...'
+        tmp_file = open(os.path.join(self._output_dir, self._logfile), 'w')
+        tmp_file.writelines(self._duplicatefiles)
+        tmp_file.close()
     
 if __name__ == '__main__':
     
@@ -70,5 +96,8 @@ if __name__ == '__main__':
     obj_dm._input_dirs = paras.get('i')
     obj_dm._output_dir = ''.join(paras.get('o'))
     obj_dm._verbose = paras.get('v')
+    obj_dm.printProlog()
     obj_dm.mergeDirs()
+    obj_dm.writeLog()
+    obj_dm.printEpilog()
     
